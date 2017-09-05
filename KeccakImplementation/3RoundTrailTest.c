4cdb9ddeabb873eefbdd3d67bab9ca377149f9ea
Luca Tansini <luca.tansini@studenti.unimi.it>
2017-09-05 17:11:53 +0200

    Aggiunti trail di esempio

--- KeccakImplementation/3RoundTrailTest.c
+++ KeccakImplementation/3RoundTrailTest.c
@@ -2,37 +2,50 @@
 
 int main(int argc, char const *argv[]) {
 
-    uint64_t state[5][5] = {
-        {0xF135E26BC449789A, 0x4D789AF135A226BC, 0x5E26BC4D78B5AF13, 0x89AF135E268FC4D7, 0x6BC4D789AF1D35E2},
-        {0xF135E26BC449789A, 0x4D789AF135A226BC, 0x5E26BC4D78B5AF13, 0x89AF135E268FC4D7, 0x6BC4D789AF1D35E2},
-        {0xF135E26BC449789A, 0x4D789AF135A226BC, 0x5E26BC4D78B5AF13, 0x89AF135E268FC4D7, 0x6BC4D789AF1D35E2},
-        {0xF135E26BC4C9789A, 0x4D789AF135AA26BC, 0x5E26BC4D78B5AF13, 0x89AF135E268FC4D7, 0x6BC4D789AF1D35E2},
-        {0xF135E26BC449789A, 0x4D789AF135A226BC, 0x5E26BC4D78B5AF13, 0x89AF135E268FC4D7, 0x6BC4D789AF1D35E2}
-    };
+    uint64_t state[5][5];
+    int i,j;
+    int backwardExtended;
+    FILE *fp;
+
+    if(argc != 2){
+        printf("Error! usage: 3RoundTrailTest <first_trail_state_input>\n");
+        exit(1);
+    }
+
+    if (!(fp=fopen(argv[1],"r"))) {
+        printf("Error: '%s' no such file or directory\n",argv[1]);
+        exit(1);
+    }
+
+    for(i=0;i<5;i++)
+        for(j=0;j<5;j++)
+            fscanf(fp,"%lx", &state[i][j]);
+    fscanf(fp,"%d",&backwardExtended);
+    fclose(fp);
 
     //Apllico i 3 round, tranne Chi che viene considerata identità e Iota che non fa nulla e vedo cosa succede
-    printf("a0:\n");
+    if(backwardExtended){
+        printf("a0(before λ0):\n");
+        PrintState(state);
+        Keccak_f_Theta(state);
+        Keccak_f_Rho(state);
+        Keccak_f_Pi(state);
+    }
+    printf("b0(before χ0):\n");
+    PrintState(state);
+    printf("a1(before λ1):\n");
     PrintState(state);
     Keccak_f_Theta(state);
     Keccak_f_Rho(state);
     Keccak_f_Pi(state);
-    printf("b0:\n");
+    printf("b1(before χ1):\n");
     PrintState(state);
-    printf("a1:\n");
-    PrintState(state);
-    Keccak_f_Theta(state);
-    Keccak_f_Rho(state);
-    Keccak_f_Pi(state);
-    printf("b1:\n");
-    PrintState(state);
-    printf("a2:\n");
+    printf("a2(before λ2):\n");
     PrintState(state);
     Keccak_f_Theta(state);
     Keccak_f_Rho(state);
     Keccak_f_Pi(state);
-    printf("b2:\n");
-    PrintState(state);
-    printf("a3:\n");
+    printf("b2(before χ2):\n");
     PrintState(state);
 
     return 0;
